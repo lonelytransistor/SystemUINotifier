c86ad2831c13320aaf91595a935965e9135c4341
lonelytransistor <lonelytransistor@protonmail.com>
2024-02-21 12:32:20 +0100

    Hide unnecessary action buttons in selector activity

--- app/src/main/java/net/lonelytransistor/notificationinsystem/ui/ApkSelectorActivity.java
+++ app/src/main/java/net/lonelytransistor/notificationinsystem/ui/ApkSelectorActivity.java
@@ -4,6 +4,10 @@ import net.lonelytransistor.commonlib.apkselect.SelectorServicedActivity;
 
 public class ApkSelectorActivity extends SelectorServicedActivity {
     @Override
+    protected boolean isButtonVisible(Button btn) {
+        return btn == Button.RELOAD || btn == Button.SORT;
+    }
+    @Override
     protected String getHeader() {
         return "Monitored Apps";
     }
